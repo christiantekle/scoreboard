2fd8733317162e212c9ca538cafe1bc78615b77f
christiantekle <christiantekle@users.noreply.github.com>
2020-07-10 04:47:27 +0400

    mongoose installed and connected

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,13 @@
-const Koa = require('koa');
+const Koa = require("koa");
 const app = new Koa();
-const bodyParser = require('koa-body');
+const bodyParser = require("koa-body");
+const mongoose = require('mongoose');
 
 app.use(bodyParser());
-app.use(async ctx => ctx.body = 'hello world!');
+app.use(async (ctx) => (ctx.body = "hello world!"));
 
-app.listen(8000, ()=> {
-    console.log('Server running at port 8000')
-})
+mongoose.connect("mongodb://localhost/scoreboard", { useNewUrlParser: true, useUnifiedTopology: true});
+
+app.listen(8000, () => {
+  console.log("Server running at port 8000");
+});
